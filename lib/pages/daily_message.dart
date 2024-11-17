c3c890c0ed540f913cd7a36f2910d6934695784a
Baranidharan <baranidharanofficial@gmail.com>
2024-11-17 07:58:22 +0530

    m

--- lib/pages/daily_message.dart
+++ lib/pages/daily_message.dart
@@ -20,86 +20,78 @@ class _DailyMessageScreenState extends State<DailyMessageScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF0E0E0E),
-      body: Stack(
-        children: [
-          Center(
-            child: Padding(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Padding(
               padding: EdgeInsets.all(sizeConfig.large),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: sizeConfig.xxxxl,
-                    ),
-                    child: Text(
-                      widget.message.title,
-                      textAlign: TextAlign.center,
-                      style: textConfig.title.copyWith(
-                        color: Colors.white,
-                        fontSize: sizeConfig.xxxxl,
-                      ),
+                  Text(
+                    widget.message.title,
+                    textAlign: TextAlign.left,
+                    style: textConfig.title.copyWith(
+                      color: Colors.white,
+                      fontSize: sizeConfig.xxxxl,
                     ),
                   ),
                   SizedBox(
                     height: sizeConfig.xl,
                   ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: sizeConfig.xxxxl,
-                    ),
-                    child: Text(
-                      widget.message.quote,
-                      textAlign: TextAlign.center,
-                      style: textConfig.large.copyWith(
-                        color: Colors.white,
-                        fontSize: sizeConfig.xl,
-                      ),
+                  Text(
+                    widget.message.quote,
+                    textAlign: TextAlign.left,
+                    style: textConfig.large.copyWith(
+                      color: Colors.white,
+                      fontSize: sizeConfig.xl,
                     ),
                   ),
                 ],
               ),
             ),
-          ),
-          Positioned(
-            bottom: sizeConfig.xxxxxl * 2,
-            child: SizedBox(
-              width: MediaQuery.sizeOf(context).width,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Center(
-                    child: FilledButton(
-                      onPressed: () async {
-                        final SharedPreferences prefs =
-                            await SharedPreferences.getInstance();
+            Positioned(
+              bottom: sizeConfig.xxxxxl * 2,
+              child: SizedBox(
+                width: MediaQuery.sizeOf(context).width,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: FilledButton(
+                        onPressed: () async {
+                          final SharedPreferences prefs =
+                              await SharedPreferences.getInstance();
 
-                        List<String>? claimed = prefs.getStringList('claimed');
+                          List<String>? claimed =
+                              prefs.getStringList('claimed');
 
-                        if (claimed != null) {
-                          prefs.setStringList(
-                            'claimed',
-                            [...claimed, widget.message.quote],
-                          );
-                        }
-                        setState(() {});
+                          if (claimed != null) {
+                            prefs.setStringList(
+                              'claimed',
+                              [...claimed, widget.message.quote],
+                            );
+                          }
+                          setState(() {});
 
-                        context.pop();
-                      },
-                      style: FilledButton.styleFrom(
-                        backgroundColor: Colors.white,
-                      ),
-                      child: Text(
-                        "Claim Message",
-                        style: textConfig.large,
+                          context.pop();
+                        },
+                        style: FilledButton.styleFrom(
+                          backgroundColor: Colors.white,
+                        ),
+                        child: Text(
+                          "Claim Message",
+                          style: textConfig.large,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
